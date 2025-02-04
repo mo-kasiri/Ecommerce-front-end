e49ae4e03b514dba848c9ab9f5ad9fcc6f20b2e7
mo-kasiri <mkgh7030@gmail.com>
2025-02-04 16:56:57 +0330

    update dashboard Items type

--- app/dashboard/seller/TrackableItems.tsx
+++ app/dashboard/seller/TrackableItems.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import { ItemStatus } from "../../../types/dashboardTypes"
+//import { ItemStatus } from "../../../types/dashboardTypes"
 
 const TrackableItems = async () => {
-    const res = await fetch("'https://dummyjson.com/products'");
-    const Items: ItemStatus[] = await res.json();
-    return (
-        <>
-            <thead>
-                <tr>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <td></td>
-                <td></td>
-            </tbody>
-        </>
-    )
-}
+  //const res = await fetch("'https://dummyjson.com/products'");
+  //const Items: ItemStatus[] = await res.json();
+  return (
+    <>
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <td></td>
+        <td></td>
+      </tbody>
+    </>
+  );
+};
 
-export default TrackableItems
+export default TrackableItems;
